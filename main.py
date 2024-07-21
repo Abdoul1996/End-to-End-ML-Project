ce89bb93ab4495dbb8c9bd94feb354457c046963
aabdillahi@mail.sfsu.edu <aabdillahi@mail.sfsu.edu>
2024-07-20 22:46:16 -0700

    Finished Data Validation

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
 
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     
     logger.info(f">>>> stage {STAGE_NAME} started <<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    data_ingestion= DataIngestionTrainingPipeline()
+    data_ingestion.main()
     logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
+STAGE_NAME_Validation = "Data Validation  Stage"
+try:
+    
+    logger.info(f">>>> stage {STAGE_NAME_Validation} started <<<<")
+    data_validation= DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>> stage {STAGE_NAME_Validation} completed <<<<\n\n")
 except Exception as e:
     logger.exception(e)
     raise e 
